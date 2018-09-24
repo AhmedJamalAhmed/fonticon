eb764b98fd287aa97902c9e5323deed0a47e3501
Ahmed Gamal <abojemyeg@gmail.com>
2018-09-24 06:40:47 +0200

    Update Demo.java
    just add a filter for icons based on the enum name or the char of the icon

--- src/main/java/com/adr/fonticon/Demo.java
+++ src/main/java/com/adr/fonticon/Demo.java
@@ -22,6 +22,7 @@ import com.adr.fonticon.decorator.Rotate;
 import com.adr.fonticon.decorator.ShadowHigh;
 import com.adr.fonticon.decorator.ShadowHole;
 import com.adr.fonticon.decorator.Shine;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -32,55 +33,57 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
- *
  * @author adrian
  */
 public class Demo extends Application {
 
-    @Override public void start(Stage stage) {
-        
+    @Override
+    public void start(Stage stage) {
+
         TabPane tabpane = new TabPane();
-        
+
         FlowPane flow = new FlowPane();
         flow.setVgap(6);
         flow.setHgap(6);
         flow.setPadding(new Insets(6));
         flow.setPrefWrapLength(100); // preferred width = 300
         flow.setPrefSize(650.0, 500.0);
-        
+
         flow.getChildren().addAll(
                 createButton(IconBuilder.create(IonIcons.ION_LOADING_A, 48.0).apply(new Rotate()).build()),
                 createButton(IconBuilder.create(FontAwesome.FA_ANDROID, 48.0).apply(new FillPaint(Color.RED.darker())).build()),
                 createButton(IconBuilder.create(FontAwesome.FA_BANK, 48.0).apply(new ShadowHole(Color.MEDIUMBLUE)).build()),
                 createButton(IconBuilder.create(FontAwesome.FA_APPLE, 48.0).apply(new ShadowHole(Color.WHITE)).build()),
                 createButton(IconBuilder.create(FontAwesome.FA_BELL, 48.0).apply(new ShadowHigh()).color(Color.LIGHTGREY).build()),
-                createButton(IconBuilder.create(FontAwesome.FA_FILTER, 48.0).apply(new ShadowHigh()).color(Color.GREEN).build()),                
+                createButton(IconBuilder.create(FontAwesome.FA_FILTER, 48.0).apply(new ShadowHigh()).color(Color.GREEN).build()),
                 createButton(IconBuilder.create(FontAwesome.FA_BOMB, 48.0).apply(new ShadowHigh()).color(Color.RED).build()),
                 createButton(IconBuilder.create(IonIcons.ION_ALERT, 48.0).apply(new ShadowHigh()).color(Color.BLUE).build()),
                 createButton(IconBuilder.create(OpenIconic.ACCOUNT_LOGIN, 48.0).apply(new ShadowHole(Color.GRAY)).build()),
                 createButton(IconBuilder.create(Octicons.OCTICON_ALERT, 48.0).apply(new ShadowHole(Color.YELLOW)).build()),
-                createButton(IconBuilder.create(WeatherIcons.WI_CLOUDY, 48.0).apply(new ShadowHole(Color.WHITE)).build()),                
+                createButton(IconBuilder.create(WeatherIcons.WI_CLOUDY, 48.0).apply(new ShadowHole(Color.WHITE)).build()),
 //                createButton("Stacked test 1", new StackPane(
 //                        IconBuilder.create(FontAwesome.FA_SQUARE, 48.0).classes("fi-icondarkblue", "fi-stack-base").build(),
 //                        IconBuilder.create(FontAwesome.FA_REFRESH, 32.0).classes("fi-iconwhite", "fi-plain").build())),
-                createButton("Stacked test 2", new StackPane(                        
+                createButton("Stacked test 2", new StackPane(
                         IconBuilder.create(FontAwesome.FA_CIRCLE_THIN, 48.0).apply(new Shine(Color.LIME)).build(),
                         new Label("31")))
-                );
-        
+        );
+
         ScrollPane p = new ScrollPane();
         p.setContent(flow);
         Tab t = new Tab("Styles");
         t.setClosable(false);
         t.setContent(p);
         tabpane.getTabs().add(t);
-        
+
         addFontIcon(tabpane, FontAwesome.class.getSimpleName(), FontAwesome.values());
         addFontIcon(tabpane, IonIcons.class.getSimpleName(), IonIcons.values());
         addFontIcon(tabpane, Octicons.class.getSimpleName(), Octicons.values());
@@ -89,36 +92,54 @@ public class Demo extends Application {
         addFontIcon(tabpane, Material.class.getSimpleName(), Material.values());
         addFontIcon(tabpane, Holo.class.getSimpleName(), Holo.values());
 
-        Scene scene = new Scene(tabpane);        
+        Scene scene = new Scene(tabpane);
         stage.setScene(scene);
         stage.show();
     }
-    
+
     private void addFontIcon(TabPane tabpane, String name, IconFont[] icons) {
-        
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(5, 5, 5, 5));
+        vBox.setSpacing(5);
+        Tab t = new Tab(name);
+        TextField filter = new TextField();
+        filter.setPromptText("filter");
         FlowPane flow = new FlowPane();
+
+        filter.setOnKeyReleased(event -> {
+            flow.getChildren().clear();
+
+            for (IconFont icon : icons) {
+                if (icon.toString().contains(filter.getText().toUpperCase())) {
+                    flow.getChildren().add(createButton(IconBuilder.create(icon, 48.0).build()));
+                }
+            }
+        });
+
+
         flow.setVgap(6);
         flow.setHgap(6);
         flow.setPadding(new Insets(6));
         flow.setPrefWrapLength(100);
         flow.setPrefSize(650.0, 500.0);
-        
+
         for (IconFont icon : icons) {
             flow.getChildren().add(createButton(IconBuilder.create(icon, 48.0).build()));
-        }    
-        
+        }
+
         ScrollPane p = new ScrollPane();
         p.setContent(flow);
-        Tab t = new Tab(name);
+        vBox.getChildren().add(filter);
+        vBox.getChildren().add(p);
         t.setClosable(false);
-        t.setContent(p);
-        tabpane.getTabs().add(t);        
+        t.setContent(vBox);
+        tabpane.getTabs().add(t);
     }
-    
+
     private Button createButton(Node graph) {
         return createButton((String) graph.getProperties().get("ICONLABEL"), graph);
     }
-    
+
     private Button createButton(String text, Node graph) {
         Button b = new Button(text, graph);
         b.setMinSize(200.0, 120.0);
@@ -127,8 +148,8 @@ public class Demo extends Application {
         b.setContentDisplay(ContentDisplay.TOP);
         return b;
     }
-    
+
     public static void main(String[] args) {
         Application.launch(args);
-    }    
+    }
 }
